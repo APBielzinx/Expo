3b1664fb72da9e4b1675088d56a1d594f6791ef4
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-03-14 10:52:17 -0300

    ProjetoExpo

---
+++ App.js
@@ -0,0 +1,41 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { View, TextInput,TouchableOpacity } from "react-native";
+import estilo from "./src/estilos";
+import { Feather } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
+
+export default function App() {
+  return (
+    <View styles={estilo.container}>
+      <LinearGradient
+        colors={["blue", "#993399"]}
+        style={estilo.gradientBackground}
+      ></LinearGradient>
+      {/* view com as entradas de dados*/}
+      <View style={estilo.box}>
+          <View style = {estilo.viewIcons}>
+          <Feather name="user" size={24} color="black" />
+            <TextInput style = {estilo.input}  placeholder='email' />
+          </View>
+          <View style = {estilo.viewIcons}>
+           <Ionicons name="key-sharp" size={24} color="black" />
+            <TextInput style = {estilo.input} placeholder='senha' />
+           </View>
+
+           <TouchableOpacity style = {{width:'80%'}}>
+
+              <LinearGradient
+              colors = {['blue', '#993399']}
+              style={estilo.button}
+
+              >
+
+              </LinearGradient>
+
+           </TouchableOpacity>
+           
+      </View>
+    </View>
+  );
+}
