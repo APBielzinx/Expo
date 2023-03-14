69b1cd18d2211a33416a9c90fea6580ff1cb389a
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-03-14 17:35:10 -0300

    Adicionando imagem

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { View, TextInput,TouchableOpacity } from "react-native";
+import { View, TextInput, TouchableOpacity, Text, Image } from "react-native";
 import estilo from "./src/estilos";
-import { Feather } from '@expo/vector-icons'; 
-import { Ionicons } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -11,30 +11,31 @@ export default function App() {
       <LinearGradient
         colors={["blue", "#993399"]}
         style={estilo.gradientBackground}
-      ></LinearGradient>
-      {/* view com as entradas de dados*/}
+      >
+
+          <Image source = {{ uri :'https://artpoin.com/wp-content/uploads/2022/08/ESTRELA-ROSA-PNG.png'}}
+          style = {estilo.Image}
+          /> 
+
+
+      </LinearGradient>
+
+      {/* View com as entradas de dados*/}
       <View style={estilo.box}>
-          <View style = {estilo.viewIcons}>
+        <View style={estilo.viewIcons}>
           <Feather name="user" size={24} color="black" />
-            <TextInput style = {estilo.input}  placeholder='email' />
-          </View>
-          <View style = {estilo.viewIcons}>
-           <Ionicons name="key-sharp" size={24} color="black" />
-            <TextInput style = {estilo.input} placeholder='senha' />
-           </View>
+          <TextInput style={estilo.input} placeholder="E-mail" />
+        </View>
 
-           <TouchableOpacity style = {{width:'80%'}}>
-
-              <LinearGradient
-              colors = {['blue', '#5d46e2']}
-              style={estilo.button}
-
-              >
-
-              </LinearGradient>
-
-           </TouchableOpacity>
-           
+        <View style={estilo.viewIcons}>
+          <FontAwesome5 name="key" size={24} color="black" />
+          <TextInput style={estilo.input} placeholder="Senha" />
+        </View>
+        <TouchableOpacity style={{ width: "80%" }}>
+          <LinearGradient colors={['blue', '#5d46e2']} style={estilo.button}>
+            <Text style={estilo.Text}> Logar</Text>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
     </View>
   );
